9b65584c64eb59dafb1cc7e84136d3480686da7c
123lisi <361789224@qq.com>
2019-09-08 18:30:50 +0800

    index 展示

--- src/main/java/life/maijiang/community/community/mapper/QuestionMapper.java
+++ src/main/java/life/maijiang/community/community/mapper/QuestionMapper.java
@@ -1,12 +1,16 @@
 package life.maijiang.community.community.mapper;
 
 import life.maijiang.community.community.model.Question;
-import life.maijiang.community.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface QuestionMapper {
-    @Insert("insert into question(title,description,gmt_create,gmt_modified,creator,tag) values (#{title},#{description},#{gmt_create},#{gmt_modified},#{creator},#{tag})")
+    @Insert("insert into question(title,description,gmt_create,gmt_modified,creator,tag) values (#{title},#{description},#{gmtCreate},#{gmtModified},#{creator},#{tag})")
     void create(Question question);
+    @Select("select * from question order by gmt_create")
+    List<Question> list();
 }
